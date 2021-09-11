3de81d1003f36ee0a11c18489429ea1ea2a4419c
Pagli3u <89987878+Pagli3u@users.noreply.github.com>
2021-09-11 16:08:31 -0300

    Add files via upload

--- TRABALHO.py
+++ TRABALHO.py
@@ -3,19 +3,41 @@ print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
 j=1
 i=0
 h=7
-
-while (j < 9) and (h!=0):
-    
+z=j
+while (j != 6) and (h!=0):
     print(h)
     caminho = int(input())
     if (caminho == 1):
         h=h-1
-        print("Você esta na sala:", j+1)
+        j=j+1
+        print("Você esta na sala:", j)
         print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
         
-        j=j+1
+        
     elif (caminho == 2):
         h=h-1
-        print("Você esta na sala:", j+2)
-        print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
         j=j+2
+        print("Você esta na sala:", j)
+        print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
+            
+if j==6:
+    caminho = int(input())
+    print("[2] - Caminho Preto")
+    h=h-1
+    j=j+2
+else:
+    while (j<=9):
+        caminho = int(input())
+        if (caminho == 1):
+            h=h-1
+            j=j+1
+            print("Você esta na sala:", j+1)
+            print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
+        
+            
+        elif (caminho == 2):
+            h=h-1
+            j=j+2
+            print("Você esta na sala:", j+2)
+            print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
+
